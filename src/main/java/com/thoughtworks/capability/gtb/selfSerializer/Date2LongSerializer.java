ceecc83751f94aefa6d3594ff0ae176c7db473ba
jiajie <jiajie.ma@thoughtworks.com>
2020-10-20 17:58:21 +0800

    create Date2LongSerializer.java

---
+++ src/main/java/com/thoughtworks/capability/gtb/selfSerializer/Date2LongSerializer.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.capability.gtb.selfSerializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class Date2LongSerializer extends StdSerializer<Date> {
+
+    protected Date2LongSerializer() {
+        super(Date.class);
+    }
+    @Override
+    public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider provider) throws IOException {
+        jsonGenerator.writeNumber(date.getTime() / 1000); //毫秒
+    }
+}
